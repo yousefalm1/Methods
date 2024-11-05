288f7bb8ddfd0493dd76dfab30f7d115ff8bc3de
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-05 08:44:20 +0300

    Forgot to create a repo

--- src/SummationPractice.java
+++ src/SummationPractice.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class SummationPractice {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(6);
+
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(ArrayList<Integer> numbers) {
+
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
 }
